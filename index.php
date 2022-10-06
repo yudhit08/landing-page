4aefae8ed070d0bece28f895fa40383fbdc498f6
faiz-hadi <www.faizhadi123@gmail.com>
2022-10-06 09:13:42 +0700

    Added header and made php file

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
+<?php
+include_once 'header.php';
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,9 +8,14 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
+    <script src="https://cdn.tailwindcss.com"></script>
     <title>Landing Page</title>
 </head>
 <body>
-    <h1>Landing Page</h1>
+    <img class="h-auto w-screen" src="img/pc.jpg" alt="">
+    <div class="bg-blue-100 h-screen ">
+        <h1 class="">Landing Page</h1>
+    </div>
+    
 </body>
 </html>
